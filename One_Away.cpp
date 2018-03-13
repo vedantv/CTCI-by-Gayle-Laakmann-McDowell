77c096a062f7eeaf7f1d53fb130fde87f7429b9c
vedantv <vedant1966@gmail.com>
2018-03-13 23:24:21 +0530

    one_away (beginning)

---
+++ One_Away.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s1,s2;
+	cout<<"enter string 1 \n";
+	getline(cin,s1);
+	cout<<"enter strign 2 \n";
+	getline(cin,s2);
+	if(s1.length()==s2.length())
+    {
+
+    }
+    else if(s1.length()==s2.length()+1)
+    {
+
+    }
+    else if(s1.length()==s2.length()-1)
+    {
+
+    }
+
+	s1.length();
+	return 0;
+}
