1d69782765f8dfe9779b105c1c00c279308ed676
vedantv <vedant1966@gmail.com>
2018-03-15 23:53:12 +0530

    improving algo

--- One_Away.cpp
+++ One_Away.cpp
@@ -8,10 +8,27 @@ int main()
 	getline(cin,s1);
 	cout<<"enter strign 2 \n";
 	getline(cin,s2);
+	int ctr=0,i,j,label[20];
+	for(i=0;i<s2.length();i++)
+	{
+		label[i]=0;
+	}
 	if(s1.length()==s2.length())
     {
-
+    	for(i=0;i<s1.length();i++)
+    	{
+    		for(j=0;j<s2.length();j++)
+    		{
+    			if(s1[i]==s2[j] && label[j]==0)
+    			{
+    				ctr++;
+    				label[j]+=1;
+    			}
+    		}
+    	}
     }
+    if(ctr==s2.length())
+    	cout<<"given strings are palindrome of each other already";
     else if(s1.length()==s2.length()+1)
     {
 
